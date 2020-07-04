2bcee075814a645d67f28d4be6ba76b1f65b1d3d
Lucia Cavagna <cavagnalucia@gmail.com>
2020-07-04 18:05:47 -0300

    showAll.blade.php - revisar logica

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     {{-- <link rel="shortcut icon" href="{{asset('img/vintrash.ico')}}" type="image/x-icon"> --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -15,5 +16,6 @@
     <link rel="stylesheet" href="{{ asset('css/bienvenida_mobile.css') }}">
     <link rel="stylesheet" href="{{ asset('css/categorias_index.css') }}">
     <link rel="stylesheet" href="{{ asset('css/formulariosProd.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/products.css') }}">
     <title>Jorge Importadora</title>
 </head>
