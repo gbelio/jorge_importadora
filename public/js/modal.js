a29972ba19329ec68afc67cdef3a121b3aac807e
Lucia Cavagna <cavagnalucia@gmail.com>
2020-07-05 20:36:19 -0300

    galeria de imagenes - producto individual

--- public/js/modal.js
+++ public/js/modal.js
@@ -11,14 +11,14 @@ abrir.addEventListener('click', function(){
 cerrar.addEventListener('click', function(){
     modal.style.display='none';
 });
-
+getElementById('"")
 window.addEventListener('click', function(e){
     if(e.target == flex){
         modal.style.display='none';
     }
 }); */
 
-let modal = document.getElementById('miModal');
+/* let modal = document.getElementById('miModal');
 let flex = document.getElementById('flex');
 let abrir = document.getElementById('abrir');
 let cerrar = document.getElementById('close');
@@ -54,4 +54,75 @@ function showDivs(n) {
     x[i].style.display = "none"; 
   }
   x[slideIndex-1].style.display = "block"; 
+} */
+
+/* let box = document.getElementById('box_ppal');
+let selection = [document.getElementById('galery');]
+let selector = selection.src;
+console.log(selector, box);
+
+selection.addEventListener('click', function(e){
+    console.log(selector);
+    console.log(box);
+    if(e.target == selection){
+        box.src=selector;
+        console.log(selector);
+        console.log(box);
+    }
+}); */
+
+
+let box = document.getElementById("box_ppal");
+let that = document.getElementById("galery");
+let photos = document.getElementsByClassName("img_gallery");
+let selection = [];
+
+for (i=0; i<photos.length; i++){
+    selection.push(photos[i])/* .src */;
+};
+
+/* console.log(selection); */
+
+for(i=0;i<selection.length;i++){
+    console.log(selection[i]);
+    selection[i].addEventListener('click',function(e){
+        console.log(e.path[0].src);
+        console.log("click");
+        console.log(box.src);
+        if(e.path[0].src !== box.src){
+            box.src=e.path[0].src;
+        }
+    });
 }
+/* that.addEventListener('click',function(){
+    console.log("click");
+}) */
+
+
+
+
+/*  for(i=0; i< selection.length; i++){
+    console.log(selection[i]);
+     selection[i].addEventListener('click', function(){
+         
+        if(selection[i].src != box.src){
+            
+            box.src = selection[i].src;
+        }
+     }); */
+    /* selection[i].addEventListener('click',function(e){
+        console.log(selection[i]); */
+        /* console.log(box.src); */
+    /* }); */
+/* };  */
+/* console.log(selection);
+console.log(that);
+console.log(selection.src); */
+/* for(i=0; i<that.length; i++){
+    that.addEventListener('click',function(){
+        if(that.src === selection.src){
+            box.src = selection.src
+        };
+    });
+}; */
+
