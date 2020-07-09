285c8d839100d9eb4cc38704b8d721763add65e7
gbelio <gastonb.exe@gmail.com>
2020-07-09 19:58:07 -0300

    spaces

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,12 @@ Auth::routes();
 
 Route::get('/','ProductController@showAll');
 
-Route::get('/faq', 'FaqController@index');
+Route::get('/faq','FaqController@index');
 
 Route::group(['prefix'=>'profile'], function(){
     
-    Route::get('/', 'ProfileController@index');
-    Route::get('/{id}', 'ProfileController@show');
+    Route::get('/','ProfileController@index');
+    Route::get('/{id}','ProfileController@show');
 });
 
 //Categorias
@@ -23,7 +23,7 @@ Route::group(['prefix'=>'categorias'], function(){
     Route::get('/cargar','CategoryController@create'); //va a llevar al formulario de carga de categoria (solo administrador)
     Route::post('/cargar','CategoryController@store'); //va a guardar el categoria en la base de datos (solo administrador)
     Route::get('/{id}','CategoryController@show');
-    Route::patch('/{id}/editar', 'CategoryController@update'); //va a editar en la base de datos
+    Route::patch('/{id}/editar','CategoryController@update'); //va a editar en la base de datos
 });
 
 //SubCategorias
@@ -32,7 +32,7 @@ Route::group(['prefix'=>'subcategorias'], function(){
 
     Route::get('/cargar','SubcategoryController@create'); //al formulario de carga de subcategoria (solo administrador)
     Route::post('/cargar','SubcategoryController@store'); //va a guardar el subcategoria en la base de datos (solo administrador)
-    Route::patch('/{id}/editar', 'SubcategoryController@update'); //va a editar en la base de datos
+    Route::patch('/{id}/editar','SubcategoryController@update'); //va a editar en la base de datos
 });
 
 //Slider
@@ -41,36 +41,36 @@ Route::group(['prefix'=>'slider'], function(){
 
     Route::get('/cargar','SliderController@create'); //al formulario de carga de subcategoria (solo administrador)
     Route::post('/cargar','SliderController@store'); //va a guardar el subcategoria en la base de datos (solo administrador)
-    Route::patch('/{id}/editar', 'SliderController@update'); //va a editar en la base de datos
+    Route::patch('/{id}/editar','SliderController@update'); //va a editar en la base de datos
 });
 
 //Productos
 
 Route::group(['prefix'=>'productos'], function(){
 
-    Route::get('/busqueda', 'ProductController@search');    
+    Route::get('/busqueda','ProductController@search');    
     Route::get('/cargar','ProductController@create'); //va a llevar al formulario de carga de producto
     Route::post('/cargar','ProductController@store'); //va a guardar el producto en la base de datos
     Route::get('/categoria/{id}','ProductController@index'); //va a mostrar todos los productos segun el ID de categoria.    
-    Route::get('/editar/{id}', 'ProductController@edit'); //va a llevar al formulario de edición
-    Route::patch('/editar/{id}', 'ProductController@update'); //va a editar en la base de datos
+    Route::get('/editar/{id}','ProductController@edit'); //va a llevar al formulario de edición
+    Route::patch('/editar/{id}','ProductController@update'); //va a editar en la base de datos
     Route::delete('/editar/{id}','ProductController@destroy');
-    Route::get('/', 'ProductController@showAll'); //va a mostrar las fotos y detalle de un producto
-    Route::get('/{id}', 'ProductController@show'); //va a mostrar las fotos y detalle de un producto
+    Route::get('/','ProductController@showAll'); //va a mostrar las fotos y detalle de un producto
+    Route::get('/{id}','ProductController@show'); //va a mostrar las fotos y detalle de un producto
 });
 
 //Multimedia
 
 Route::get('/productos/usuario/cargar_imagen/{id}','MultimediaController@create');
 Route::post('/productos/usuario/cargar_imagen/{id}','MultimediaController@store');
-Route::get('/{id}/editar', 'MultimediaController@edit'); 
-Route::patch('/{id}/editar', 'MultimediaController@update'); 
+Route::get('/{id}/editar','MultimediaController@edit'); 
+Route::patch('/{id}/editar','MultimediaController@update'); 
 Route::delete('/productos/usuario/cargar_imagen/{id}','MultimediaController@destroy');
 
-Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('logout','\App\Http\Controllers\Auth\LoginController@logout');
 
-Route::resource('category', 'CategoryController');
+Route::resource('category','CategoryController');
 
-Route::resource('subcategory', 'SubcategoryController');
+Route::resource('subcategory','SubcategoryController');
 
-Route::resource('sliders', 'SliderController');
+Route::resource('sliders','SliderController');
