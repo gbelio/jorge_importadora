7c825bbadff764a981b3e618721b70b6a0bb9846
Lucia Cavagna <cavagnalucia@gmail.com>
2020-07-11 21:42:30 -0300

    agregar fotos y text area en el form de productos

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -5,7 +5,7 @@
     <section class="offset-0 col-11 form-categorias">
         <div class="agregarProducto">
             <div style="display:flex; flex-direction:row; justify-content:space-between">
-                <h3 style="display:inline-block">Agregar Producto</h3>
+                <h3 style="display:inline-block; font-family:'Raleway'; font-weight:bold;">Agregar Producto</h3>
                 <button id="botonFormProd" style="font-size: 0px; background-color: white; color: black;" class="pull-right"><i class="fa fa-plus-square-o pull-right" style="font-size:30px; margin:0 !important"></i></button>
                 <button id="botonFormProd1" style="font-size: 0px; background-color: white; color: black; display:none" class="pull-right"><i class="fa fa-minus-square-o" style="font-size:30px"></i></button>
             </div>
@@ -17,14 +17,7 @@
                     <div class=form-group style="display:none;">
                         <input required type="text" name= "user_id" value="{{Auth::user()->id}}">
                     </div>
-                    <div class="form-group">
-                        <label for="producto"><strong> Nombre del producto</strong></label>
-                        <input required type="text" name="name" value="{{ old("name") }}" class="form-control">
-                    </div>
-                    @error('name')
-                    <span class="errors">{{ $message }}</span>
-                    @enderror
-        
+
                     <div class="form-group">
                         <label for="code"><strong> Código</strong></label>
                         <input required type="text" name="code" value="{{ old("code") }}" class="form-control" maxlength="190">
@@ -32,20 +25,28 @@
                     @error('code')
                     <span class="errors">{{ $message }}</span>
                     @enderror
-        
+
                     <div class="form-group">
-                        <label for="descripcion"><strong> Descripción del producto</strong></label>
-                        <input required type="text" name="description" value="{{ old("description") }}" class="form-control" maxlength="190">
+                        <label for="producto"><strong> Nombre del producto</strong></label>
+                        <input required type="text" name="name" value="{{ old("name") }}" class="form-control">
                     </div>
-                    @error('description')
+                    @error('name')
                     <span class="errors">{{ $message }}</span>
-                    @enderror
-        
+                    @enderror        
+                        
                     <div class="form-group">
                         <label for="resume"><strong>Resumen del producto</strong> </label>
                         <input required type="text" name="resume" value="{{ old("resume") }}" class="form-control" maxlength="60">
                     </div>
                     @error('resume')
+                    <span class="errors">{{ $message }}</span>
+                    @enderror
+
+                    <div class="form-group">
+                        <label for="descripcion"><strong> Descripción del producto</strong></label>
+                        <textarea style="resize:none;" required type="text" name="description" value="{{ old("description") }}" class="form-control" maxlength="190"> </textarea>
+                    </div>
+                    @error('description')
                     <span class="errors">{{ $message }}</span>
                     @enderror
         
@@ -90,7 +91,7 @@
                     {{-- Si el ID de la categoría es igual al catgory_id de subcategoría, que muestre las subcategorías con ese ID --}}
         
                     <div class="button">
-                        <label for="poster" class="add_img"><strong>Selecionar Imagen</strong> </label>
+                        <label for="poster" class="add_img"><strong>Selecione una imagen de portada</strong> </label>
                         <input required class="" type="file" name="cover">
                     </div>
                     @error('cover')
@@ -124,8 +125,9 @@
                             <th>Cover</th>
                             <th>Categoría</th>
                             <th>Subcategoría</th>
-                            <th></th>
-                            <th></th>
+                            <th>Fotos</th>
+                            <th>Editar</th>
+                            <th style="color:red;">Borrar</th>
                         </thead>
                         <tbody>
                             @if($productos ?? ''->count())  
@@ -145,10 +147,15 @@
                                 
                                 <td>{{$producto->category->name}}</td>
                                 <td>{{$producto->subcategory->name}}</td>
-                            
+                                                           
+                                <td style="text-align:center"><a class="btn btn-secondary btn-sm" href="{{action('MultimediaController@create', $producto->id)}}">
+                                    <i class="fa fa-camera" style="font-size:16px"></i>
+                                </a></td>
+
                                 <td style="text-align:center"><a class="btn btn-primary btn-sm" href="{{action('ProductController@edit', $producto->id)}}">
                                     <i class="fa fa-pencil" style="font-size:16px"></i>
                                 </a></td>
+
                                 <td style="text-align:center">
         
                                 <form action="{{action('ProductController@destroy', $producto->id)}}" method="post">
