d60d46818a3b3cbdc82e8c9488e56d91a0c2047c
Lucia Cavagna <cavagnalucia@gmail.com>
2020-07-19 19:03:50 -0300

    fixes

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -90,10 +90,10 @@
     {{-- Lista de productos --}}
     <div class="col-11 form-categorias1">
         <div id="listaProductos" class="panel panel-default">
-            <div class="panel-body">
+            <div class="panel-body panel-productos">
                 <div class="pull-left"><h3>Lista Productos</h3>
                 </div>
-                <div class="table-container">
+                <div class="table-container table_productos">
                     <table id="mytable" class="table table-bordered table-striped">
                         <thead>
                             <th>Id</th>
