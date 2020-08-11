5f579507f98e0c4294b16cf5c9be6bedeea505bb
gbelio <gastonb.exe@gmail.com>
2020-08-10 21:47:51 -0300

    Se eliminó show de categorías, identación

--- resources/views/subcategorias/edit.blade.php
+++ resources/views/subcategorias/edit.blade.php
@@ -1,13 +1,10 @@
 @extends('layouts.master')
 @section('content')
-
 <div class="" style="min-height:450px; margin-top:125px;">
-
     <div id="listaCategorias" class="offset-2 col-8 form-categorias">
         <div style="display:flex; flex-direction:row; justify-content:space-between">
             <h3 style="display:inline-block">Editar Subcategoría</h3>
         </div>
-
         <form action="{{action('CategoryController@update', $subcategoria->id)}}" method="post">
             <br>
             @csrf
@@ -18,7 +15,6 @@
             </div>
             <div class="form-group">
                 <label for="name"><strong> Categoría a la que pertenece </strong></label>
-
                 <select class="form-control" name="category_id">
                     @isset($categorias)
                         @foreach($categorias as $categoria)
@@ -26,20 +22,13 @@
                         @endforeach
                     @endisset
                 </select>
-
             </div>
-            
-            
             <br>
             <div>
                 <a href="/subcategorias/cargar" class="btn btn-info btn-sm boton-eliminar" role="button" style="margin:2% 0%; background-color:#007BFF;border-color:#007BFF;">Volver</a>
                 <input type="submit" class="btn btn-info btn-sm boton-eliminar" style="margin:2% 0%; background-color:#007BFF;border-color:#007BFF;" value="Confirmar Cambios">
             </div>
         </form>
-
-       
     </div>
-
 </div>
-
 @endsection
