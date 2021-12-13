a2d4387f60ea53dc82e65a0bac8c4a0b4aefa623
gbelio <gastonb.exe@gmail.com>
2021-12-13 00:33:30 -0300

    without register

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 
-Auth::routes();
+Auth::routes(["register" => false]);
 
 Route::get('/','ProductController@showAll');
 
