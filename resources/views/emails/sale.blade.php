cc633eef85aaba31351a305a5b1730faa8060f97
gbelio <gastonb.exe@gmail.com>
2021-12-27 18:44:56 -0300

    mail fix

--- resources/views/emails/sale.blade.php
+++ resources/views/emails/sale.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 <h1>PEDIDO ENTRANTE</h1>
-<h2>Número de orden: {{$order->id}}</h2>
+<h2>Número de orden: <a href="http://www.importadorajorge.com.ar/compras/detalle/{{$order->id}}">{{$order->id}}</a></h2>
 <h2>Total: ${{$order->total}}</h2>
 <br>
 <h3>Nombre: {{$order->user->name}} {{$order->user->last_name}}</h3>
