5d5e02e3836fbdca445a1ad31c5ac3419916240c
Lucia Cavagna <cavagnalucia@gmail.com>
2022-06-14 22:32:02 -0300

    redirect to home after a purchase

--- public/js/alert.js
+++ public/js/alert.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
                             'success'
                           )
                         .then ((result) => {
-                            location.redirect();
+                            location.replace('/');
                         });
                     }
                 })
