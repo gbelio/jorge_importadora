9dda5a9d3559a4ef3783981fe2e6dc19bb70613c
gbelio <gastonb.exe@gmail.com>
2023-02-22 15:49:11 -0300

    import all css files in one

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -7,30 +7,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}" >
-    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}" >
-    <link rel="stylesheet" href="{{ asset('css/navBar.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/fuentesYmedidas.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/index_mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/login_mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/register_mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/faq_mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bienvenida_mobile.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/categorias_index.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/formulariosProd.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/products.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/photos.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/cat_prods.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/dropdown_subcategorias.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/orders.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/orderDetail.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/userOrders.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/status.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/perfil.css')}}" >
-    <link rel="stylesheet" href="{{ asset('css/botones.css')}}" >
+    <link rel="stylesheet" href="{{ asset('css/all.css?v_01') }}" >
     <title>Importadora Jorge</title>
 </head>
