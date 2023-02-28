306fd87e33c1f16f6fac9d583e829c9755830b0b
xer09 <48607502+gbelio@users.noreply.github.com>
2023-02-28 17:15:23 -0300

    Update head.blade.php

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -7,6 +7,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/all.css?v_01') }}" >
+    <link rel="stylesheet" href="{{ asset('css/all.css?v_02') }}" >
     <title>Importadora Jorge</title>
 </head>
